9672a706111d66ee35a68dbf6674dced22b70c68
Harald Fernengel <harry@kdevelop.org>
2020-06-26 21:25:54 +0200

    ffmpeg: Fix finding snappy
    Since snappy doesn't install a pkg-config file, ffmpeg wouldn't find it
    when brew is installed in a custom location. Override CFLAGS and LDFLAGS
    to let configure know where snappy is installed

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -3,7 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-4.3.tar.xz"
   sha256 "1d0ad06484f44bcb97eba5e93c40bcb893890f9f64aeb43e46cd9bb4cbd6795d"
-  revision 1
+  revision 2
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
@@ -50,6 +50,8 @@ class Ffmpeg < Formula
   uses_from_macos "zlib"
 
   def install
+    ENV.append "CFLAGS", "-I#{Formula["snappy"].opt_include}"
+    ENV.append "LDFLAGS", "-L#{Formula["snappy"].opt_lib}"
     args = %W[
       --prefix=#{prefix}
       --enable-shared
